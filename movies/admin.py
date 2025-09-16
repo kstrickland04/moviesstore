dd88772307703e91c7ae234460cb8502cb462c91
kstrickland04 <kristianstrickland.kfs@gmail.com>
2025-09-16 16:02:50 -0400

    Second version

--- movies/admin.py
+++ movies/admin.py
@@ -5,8 +5,11 @@ from django.contrib import admin
 from .models import Movie, Review
 
 class MovieAdmin(admin.ModelAdmin):
-    ordering = ['name']
-    search_fields = ['name']
+    ordering = ["name"]
+    search_fields = ["name"]
+    list_display = ("id", "name", "stock", "price")
+    list_editable = ("stock", "price")
+    list_filter = ("stock",)   
 
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Review)
--- movies/models.py
+++ movies/models.py
@@ -9,9 +9,11 @@ class Movie(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     image = models.ImageField(upload_to='movie_images/')
+    stock = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return str(self.id) + ' - ' + self.name
+    
 
 class Review(models.Model):
     id = models.AutoField(primary_key=True)
